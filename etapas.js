2a50c63ec303612c101389b69fbf8ffd6713f097
eadriano55 <e_adriano55@hotmail.com>
2021-09-06 18:28:06 -0300

    projeto de urna eletrônica

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Gaara',
+                partido: 'ABC',
+                fotos:[
+                    {url:'gaara.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Rock Lee',
+                partido: 'DEF',
+                fotos:[
+                    {url: 'rock-lee.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Naruto',
+                partido: 'ABC',
+                vice: 'Kakashi',
+                fotos: [
+                    {url: 'naruto.jpg', legenda: 'Prefeito'},
+                    {url: 'kakashi.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Sasuke',
+                partido: 'DEF',
+                vice: 'Sakura',
+                fotos: [
+                    {url: 'sasuke.jpg', legenda: 'Prefeito'},
+                    {url: 'sakura.jpg', legenda: "Vice-Prefeito", small: true}
+                ]
+            },
+        ]
+    }
+];
